491bda5e6e968cfce8aa6f9cc9d3fb7293237dda
Shadow_fdz <54904324+LuanGamer33@users.noreply.github.com>
2026-05-09 21:36:03 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -105,14 +105,6 @@ const Minijuegos = () => (
 const Biblioteca = () => (
   <main className="main-content">
     <h1 className="title">Biblioteca</h1>
-    <div className="cards-grid cols-3">
-      <Card type="blue" number="1" title="1er Año" actionText="Ver Contenido" to="#" />
-      <Card type="white" number="2" title="2do Año" actionText="Ver Contenido" to="#" />
-      <Card type="blue" number="3" title="3er Año" actionText="Ver Contenido" to="#" />
-      <Card type="white" number="4" title="4to Año" actionText="Ver Contenido" to="#" />
-      <Card type="blue" number="5" title="5to Año" actionText="Ver Contenido" to="#" />
-      <Card type="white" icon={<img src={logoTecnico} alt="Logo Técnico" style={{ height: '80px', width: 'auto' }} />} title="Técnico" actionText="Ver Contenido" to="#" />
-    </div>
   </main>
 );
 
